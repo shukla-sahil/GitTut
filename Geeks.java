e77de7258394af11c9ec2a0c84da502953c31a79
Sahil Shukla <sahilshukla20034@gmail.com>
2024-09-03 17:33:18 +0530

    Changes Geek

--- Geeks.java
+++ Geeks.java
@@ -4,7 +4,7 @@ public class Geeks {
  
     public static void main(String[] args)
     {
-        int a = 62;
+        int a = 63;
         int b = 18;
         // It will return the sum of two arguments.
         System.out.println("The is"
